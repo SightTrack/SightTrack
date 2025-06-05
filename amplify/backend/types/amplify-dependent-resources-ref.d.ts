b5d44a7bdf1d50884be4fe4df7c08ea7f2da66f6
James Tan <jamest01230@gmail.com>
2025-06-04 21:43:02 -0400

    Volunteer hours request test (with SES)

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -25,6 +25,13 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "sendVolunteerHoursRequest": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "sightingComputerVision": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
